b89696e0ca327907ea33c6135c6e12e422dcc6c7
jhogendorn <jhogendorn@gmail.com>
2010-06-28 15:30:23 +1000

    adds in the header comments, wraps it in a closure for distribution.

--- serializeFullArray.js
+++ serializeFullArray.js
@@ -1,47 +1,63 @@
-jQuery.fn.serializeFullArray = function () {
-	// Grab a set of name:value pairs from the form dom.
-	var set = $(this).serializeArray();
-	var output = {};
+/*!
+ * jQuery serializeFullArray - v0.1 - 28/06/2010
+ * http://github.com/jhogendorn/jQuery-serializeFullArray/
+ * 
+ * Copyright (c) 2010 Joshua Hogendorn
+ *
+ *
+ * Whereas .serializeArray() serializes a form into a key:pair array, .serializeFullArray()
+ * builds it into a n-tier object, respecting form input arrays.
+ *
+ */
+ 
+(function($,undefined){
+  '$:nomunge'; // Used by YUI compressor.
+  
+	$.fn.serializeFullArray = function () {
+		// Grab a set of name:value pairs from the form dom.
+		var set = $(this).serializeArray();
+		var output = {};
 
-	for (var field in set)
-	{
-		if(!set.hasOwnProperty(field)) continue;
-
-		// Split up the field names into array tiers
-		var parts = set[field].name
-			.split(/\]|\[/)
-			.filter(function(part) {
-				return !(part == '');
-		   	});
-
-		// Start ref out at the root of the output object
-		var ref = output;
-
-		for (var segment in parts)
+		for (var field in set)
 		{
-			if(!parts.hasOwnProperty(segment)) continue;
+			if(!set.hasOwnProperty(field)) continue;
 
-			// set key for ease of use.
-			var key = parts[segment];
-			var value = {};
+			// Split up the field names into array tiers
+			var parts = set[field].name
+				.split(/\]|\[/)
+				.filter(function(part) {
+					return !(part == '');
+				});
 
-			// If we're at the last part, the value comes from the original array.
-			if (segment == parts.length - 1)
+			// Start ref out at the root of the output object
+			var ref = output;
+
+			for (var segment in parts)
 			{
-				var value = set[field].value;
+				if(!parts.hasOwnProperty(segment)) continue;
+
+				// set key for ease of use.
+				var key = parts[segment];
+				var value = {};
+
+				// If we're at the last part, the value comes from the original array.
+				if (segment == parts.length - 1)
+				{
+					var value = set[field].value;
+				}
+
+				// Create a throwaway object to merge into output.
+				var objNew = {};
+				objNew[key] = value;
+
+				// Extend output with our temp object at the depth specified by ref.
+				$.extend(true, ref, objNew);
+
+				// Reassign ref to point to this tier, so the next loop can extend it.
+				ref = ref[key];
 			}
-
-			// Create a throwaway object to merge into output.
-			var objNew = {};
-			objNew[key] = value;
-
-			// Extend output with our temp object at the depth specified by ref.
-			$.extend(true, ref, objNew);
-
-			// Reassign ref to point to this tier, so the next loop can extend it.
-			ref = ref[key];
 		}
-	}
 
-	return output;
-}
+		return output;
+	}
+})(jQuery);
