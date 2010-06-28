8197e1d21717009f365e486c3327885ff3b77de1
jhogendorn <jhogendorn@gmail.com>
2010-06-28 15:12:22 +1000

    fixes some minor syntax, more efficient, adds comments.

--- serializeFullArray.js
+++ serializeFullArray.js
@@ -1,30 +1,44 @@
 jQuery.fn.serializeFullArray = function () {
+	// Grab a set of name:value pairs from the form dom.
 	var set = $(this).serializeArray();
 	var output = {};
-	for (field in set)
+
+	for (var field in set)
 	{
+		if(!set.hasOwnProperty(field)) continue;
+
+		// Split up the field names into array tiers
 	    var parts = set[field].name
 	    	.split(/\]|\[/)
 	    	.filter(function(part) {
 		    	return !(part == '');
 		   	});
 
+		// Start ref out at the root of the output object
 	    var ref = output;
 
 	    for (var segment in parts)
 	    {
+			if(!parts.hasOwnProperty(segment)) continue;
+
+			// set key for ease of use.
 	        var key = parts[segment];
 	        var value = {};
 
-	        if (key == parts[parts.length - 1])
+	        // If we're at the last part, the value comes from the original array.
+	        if (segment == parts.length - 1)
 	        {
 	            var value = set[field].value;
 	        }
 
+	        // Create a throwaway object to merge into output.
 	        var objNew = {};
 	        objNew[key] = value;
 
+	        // Extend output with our temp object at the depth specified by ref.
 	        $.extend(true, ref, objNew);
+
+	        // Reassign ref to point to this tier, so the next loop can extend it.
 	        ref = ref[key];
 	    }
 	}
